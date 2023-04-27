56c5fd9ac823dbcbf9ff5eb652bca1123b27f9a4
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-04-27 21:04:09 +0530

    update: hero

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import Navbar from '../components/Navbar';
+import backgroundImage from './components/BackgroundImage';
+import MovieLogo from "./assets/homeTitle.webp"
+import { FaPlay } from 'react-icons/fa';
+import {AiOutlineInfoCircle} from "react-icons/ai";
 
 function Netflix() {
 
@@ -15,6 +19,23 @@ function Netflix() {
   return (
     <div>
       <Navbar isScrolled={isScrolled} />
+      <div className='hero'>
+      <img src={backgroundImage} alt='bg' className='background-image' />
+      <div className="container">
+        <div className="logo">
+          <img src={MovieLogo} alt="movie logo" />
+        </div>
+        <div className="buttons flex">
+          <button className='flex j-center a-center'>
+            <FaPlay /> play  
+          </button>
+          <button className='flex j-center a-center'>
+            <AiOutlineInfoCircle /> More Info  
+          </button>
+        </div>
+      </div>
+      </div>
+
     </div>
   )
 }
