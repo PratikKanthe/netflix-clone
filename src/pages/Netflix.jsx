94534c44b9cf11eb971a9e11aa71e0e543431eb7
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-04-27 22:14:00 +0530

    update: styles

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -52,6 +52,40 @@ const Container = styled.div`
       height: 100vh;
       width: 100vw;
     }
+    .container {
+      postion: absolute;
+      bottom: 5rem;
+      .logo {
+        width: 100%;
+        height: 100%;
+        margin-left: 5rem;
+      }
+    }
+    .buttons {
+      margin: 5rem;
+      gap: 2rem;
+      button {
+        font-size: 1.4rem;
+        gap: 1rem;
+        border-radius: 0.2rem;
+        padding: 0.5rem;
+        padding-left: 2rem;
+        padding-right: 2.4rem;
+        border: none;
+        cursor: pointer;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          opacity: 0.8;
+        }
+        &:nth-of-type(2) {
+          background-color: rgba(100, 109, 110, 0.7);
+          color: white;
+          svg {
+            font-size: 1.8rem;
+          }
+        }
+      }
+    }
   }
 `;
 
