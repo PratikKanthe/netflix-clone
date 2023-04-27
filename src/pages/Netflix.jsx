3496bcfab4a902d584021065e91fe938ee175469
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-04-27 21:13:02 +0530

    add: image

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Navbar from '../components/Navbar';
-import backgroundImage from '../components/BackgroundImage';
+import backgroundImage from '../assets/home.jpg';
 import MovieLogo from '../assets/homeTitle.webp'
 import { FaPlay } from 'react-icons/fa';
 import {AiOutlineInfoCircle} from "react-icons/ai";
