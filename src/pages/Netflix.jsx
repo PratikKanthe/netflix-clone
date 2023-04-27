a2c0829bd7b5a6a406d0f966b03f09df9f8e7cda
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-04-27 22:24:19 +0530

    update: nav

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -14,7 +14,7 @@ function Netflix() {
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
-  }
+  };
 
 
   return (
@@ -53,35 +53,37 @@ const Container = styled.div`
       width: 100vw;
     }
     .container {
-      postion: absolute;
+      position: absolute;
       bottom: 5rem;
       .logo {
-        width: 100%;
-        height: 100%;
-        margin-left: 5rem;
-      }
-    }
-    .buttons {
-      margin: 5rem;
-      gap: 2rem;
-      button {
-        font-size: 1.4rem;
-        gap: 1rem;
-        border-radius: 0.2rem;
-        padding: 0.5rem;
-        padding-left: 2rem;
-        padding-right: 2.4rem;
-        border: none;
-        cursor: pointer;
-        transition: 0.3s ease-in-out;
-        &:hover {
-          opacity: 0.8;
+        img {
+          width: 100%;
+          height: 100%;
+          margin-left: 5rem;
         }
-        &:nth-of-type(2) {
-          background-color: rgba(100, 109, 110, 0.7);
-          color: white;
-          svg {
-            font-size: 1.8rem;
+      }
+      .buttons {
+        margin: 5rem;
+        gap: 2rem;
+        button {
+          font-size: 1.4rem;
+          gap: 1rem;
+          border-radius: 0.2rem;
+          padding: 0.5rem;
+          padding-left: 2rem;
+          padding-right: 2.4rem;
+          border: none;
+          cursor: pointer;
+          transition: 0.2s ease-in-out;
+          &:hover {
+            opacity: 0.8;
+          }
+          &:nth-of-type(2) {
+            background-color: rgba(109, 109, 110, 0.7);
+            color: white;
+            svg {
+              font-size: 1.8rem;
+            }
           }
         }
       }
