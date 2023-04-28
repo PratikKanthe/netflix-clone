5a5b1409b85e327227afc07c53b58f52c96a3515
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-04-28 12:07:01 +0530

    updated: styles

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -19,5 +19,22 @@ export default function Player() {
 }
 
 const Container = styled.div`
-
+    .player {
+        width: 100vw;
+        height: 100vh;
+        .back {
+           position: absolute;
+           padding: 2rem;
+           z-index: 1;
+           svg {
+            font-size: 3rem;
+            cursor: pointer;
+           } 
+        }
+        video {
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+        }
+    }
 `;
