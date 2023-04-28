d2f8b942b1fe282be53b7431471cb94fb896799e
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-04-28 11:50:41 +0530

    update: react route

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 import { BsArrowLeft } from "react-icons/bs"
-import video from "../assets/"
+import video from "../assets/video.mp4"
+import { useNavigate } from 'react-router-dom'
 
 export default function Player() {
+    const navigate =  useNavigate();
   return (
-    <Container>Player</Container>
+    <Container>
+        <div className="player">
+            <div className="back">
+                <BsArrowLeft onClick={() => navigate(-1)} />
+            </div>
+            <video src={video} autoPlay loop controls muted></video>
+        </div>
+    </Container>
   )
 }
 
