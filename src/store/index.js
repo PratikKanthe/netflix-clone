e6a22a5e99b341217ea29d46cd6fe8bf0e6fc1de
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-04-29 10:57:03 +0530

    note: fetching movies

--- src/store/index.js
+++ src/store/index.js
@@ -22,6 +22,48 @@ export const getGenres = createAsyncThunk("netflix/genres", async()=>{
         return genres;
 });
 
+const createArrayFromRawData = (array, moviesArray,genres) => {
+array.forEach((movie) => {
+    const movieGenres = [];
+    movie.genres_ids.forEach((genre) => {
+        const name = genres.find(({id}) => id === genre);
+    });
+    if (movie.backdrop_path) {
+        moviesArray.push({
+            id: movie.id,
+            name: movie?.original_name ? movie.original_name : movie.original_title,
+            image: movie.backdrop_path,
+            genres: moviesArray.slice(0, 3),
+        });
+    }
+});
+}
+
+const getRawData = async (api, genres, paging) => {
+    const moviesArray = [];
+    for(let i=1; moviesArray.length<60 && i<10; i++) {
+       const { data: results } = await axios.get(
+        `${api}${paging? `$[i]` : "" }`
+        );
+        createArrayFromRawData(results, moviesArray, genres);
+        return moviesArray;
+    }
+};
+
+export const fetchMovies = createAsyncThunk(
+    "netflix/trending",
+    async ({type}, thunkApi) => {
+        const {
+            netflix: {genres},
+        } = thunkApi.getState();
+        return getRawData(
+            `${TMDB_BASE_URL}/trending/${type}/week?api_key=${API_KEY}`,
+            genres,
+            true
+        );
+    }
+);
+
 const NetflixSlice = createSlice({
     name: "Netflix",
     initialState,
