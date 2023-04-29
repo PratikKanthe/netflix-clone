cae659216ca6fea0657cd41d939acc39126a54b1
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-04-29 13:35:49 +0530

    initial commit

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -8,6 +8,7 @@ import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMovies, getGenres } from '../store';
+import Slider from '../components/Slider';
 
 function Netflix() {
 
@@ -49,7 +50,7 @@ function Netflix() {
         </div>
       </div>
       </div>
-
+      <Slider movies={movies} />
     </Container>
   )
 }
